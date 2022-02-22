c4f68e9256512a679ff42838e17054161f4dfa71
Eric Van <ggclarence@yahoo.com>
2022-02-22 16:15:24 -0600

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,17 @@
 import React from "react";
 
+// - Two components, `<Navbar>` and `<Home>`, are already defined in the `App.js`
+//   file. Add both of these components as _child_ components of `<App>` by
+//   including them in the return statement for the `App` function.
+
+// - Create a third component, `<About>`. The `<About>` component should also be a
+//   _child_ component of `<App>`.
+
+//   - The `<About>` component should return a `<div>` with an id of `about`. To
+//     match the screenshot, the div should contain an `<h2>` element with the text
+//     "About." However, as long as it has an id of `about`, it will pass the test!
+
+
 function NavBar() {
   return (
     <nav>
@@ -16,15 +28,21 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+function About() {
+  return (
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  )
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home/>
+      <About/>
     </div>
   );
 }
